b33cff24099c946c0bbe4f85983d5b048f63ef11
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-10-13 15:08:37 +0200

    Add files via upload

---
+++ Ponavljanje1/Ponavljanje1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, c;
+
+            Console.WriteLine("Unesi 3 dvoznamenkasta broja: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a >= b && a >= c)
+            {
+                Console.WriteLine(a);
+            }
+
+            if (b >= a && b >= c)
+            {
+                Console.WriteLine(b);
+            }
+
+            if (c >= b && c >= a)
+            {
+                Console.WriteLine(c);
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
